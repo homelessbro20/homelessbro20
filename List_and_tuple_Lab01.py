5e4078614c106bf395ebc77d82b9af7e2c509284
homelessbro20 <144569066+homelessbro20@users.noreply.github.com>
2023-09-20 20:01:15 -0500

    Add files via upload

---
+++ List_and_tuple_Lab01.py
@@ -0,0 +1,464 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Numbers = (1.0, 9,9, 10)
+
+
+# In[2]:
+
+
+Numbers
+
+
+# In[8]:
+
+
+Student_Names = ('jose', 'josh', 'tony')
+
+
+# In[6]:
+
+
+Student_Names
+
+
+# In[7]:
+
+
+type(Student_Names)
+
+
+# In[9]:
+
+
+Student_Names
+
+
+# In[10]:
+
+
+type(Student_Names)
+
+
+# In[11]:
+
+
+Student_Names{0}
+
+
+# In[12]:
+
+
+Student_Names[0]
+
+
+# In[13]:
+
+
+type(Student_Names[1])
+
+
+# In[14]:
+
+
+Student_Names = ('jose', 1, 'tony')
+
+
+# In[15]:
+
+
+type(Student_Names[1])
+
+
+# In[16]:
+
+
+Student_ID = (Name, Class, Homework, Question, Grade, Date)
+
+
+# In[17]:
+
+
+Name='Jose'
+
+
+# In[18]:
+
+
+Student_ID
+
+
+# In[19]:
+
+
+Class='CSC'
+
+
+# In[20]:
+
+
+Class
+
+
+# In[21]:
+
+
+Homework='Week_03'
+
+
+# In[22]:
+
+
+Homework
+
+
+# In[23]:
+
+
+Question=4
+
+
+# In[32]:
+
+
+Grade='Grade:10'
+
+
+# In[26]:
+
+
+Date='September 18 2023'
+
+
+# In[27]:
+
+
+Date
+
+
+# In[28]:
+
+
+Student_ID
+
+
+# In[29]:
+
+
+Student_ID = (Name, Class, Homework, Question, Grade, Date)
+
+
+# In[30]:
+
+
+Student_ID
+
+
+# In[31]:
+
+
+Student_ID[4]
+
+
+# In[33]:
+
+
+Student_ID
+
+
+# In[34]:
+
+
+Student_ID
+
+
+# In[35]:
+
+
+Student_ID[4]=weijnweiv
+
+
+# In[36]:
+
+
+Student_ID[4]=5749
+
+
+# In[37]:
+
+
+tuple_A = (0, [1,2,3],(4,5,6),7.0)
+
+
+# In[38]:
+
+
+tuple_A[1]
+
+
+# In[40]:
+
+
+type (tuple_A[1])
+
+
+# In[1]:
+
+
+#wednesday down below
+
+
+# In[2]:
+
+
+numbers=[1,2,3,4,5,6,7,8,9,10]
+
+
+# In[3]:
+
+
+numbers
+
+
+# In[4]:
+
+
+#create list for odd numbers
+
+
+# In[5]:
+
+
+odd=[1,3,5,7,9]
+
+
+# In[6]:
+
+
+type(odd)
+
+
+# In[8]:
+
+
+Name='Jose'
+
+
+# In[9]:
+
+
+Class='CSC'
+
+
+# In[10]:
+
+
+Homework='List'
+
+
+# In[11]:
+
+
+Question='List'
+
+
+# In[12]:
+
+
+Grade='sophmore'
+
+
+# In[13]:
+
+
+Date='September 20, 2023'
+
+
+# In[17]:
+
+
+Student_ID=[Name, Class, Homework, Question, Grade, Date]
+
+
+# In[15]:
+
+
+Student_ID
+
+
+# In[16]:
+
+
+type(Student_ID)
+
+
+# In[18]:
+
+
+type(Student_ID)
+
+
+# In[19]:
+
+
+Student_ID
+
+
+# In[20]:
+
+
+Student_ID[4]
+
+
+# In[21]:
+
+
+Student_ID[4]='A'
+
+
+# In[22]:
+
+
+Student_ID
+
+
+# In[23]:
+
+
+Student_ID[4]
+
+
+# In[28]:
+
+
+list.append('School')
+
+
+# In[29]:
+
+
+School='Umary'
+
+
+# In[30]:
+
+
+list.append('School')
+
+
+# In[31]:
+
+
+Student_ID.append('School')
+
+
+# In[32]:
+
+
+Student_ID
+
+
+# In[33]:
+
+
+Student_ID.remove(Grade)
+
+
+# In[34]:
+
+
+Student_ID.insert(1,'JOSE')
+
+
+# In[39]:
+
+
+Student_ID
+
+
+# In[38]:
+
+
+Student_ID.remove('List')
+
+
+# In[40]:
+
+
+Student_ID.remove('A')
+
+
+# In[41]:
+
+
+Student_ID
+
+
+# In[42]:
+
+
+Student_ID.extend('Jose')
+
+
+# In[43]:
+
+
+Student_ID
+
+
+# In[44]:
+
+
+PI=['Jose','UMARYaddress','stoneb7@zoho.com','916.620.5291']
+
+
+# In[45]:
+
+
+AI=["UMARY,Engineering,Sophomore,3,A"]
+
+
+# In[46]:
+
+
+PI.extend(AI)
+
+
+# In[47]:
+
+
+PI
+
+
+# In[48]:
+
+
+List_AA=[0,[1,2,3],(4,5,6),7.0]
+
+
+# In[49]:
+
+
+List_AA[3]
+
+
+# In[50]:
+
+
+type(List_AA[2])
+
+
+# In[51]:
+
+
+#you can in theory, but if you change the parentheses to brackets, because the tuple cannot be changed, but a list can
+
+
+# In[ ]:
+
+
+
+
